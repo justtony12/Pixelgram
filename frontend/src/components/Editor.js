06cf9c579f31d3bd0cd2a68fca0c9d39fdaf2e69
marcus <marcus.longoria12@gmail.com>
2021-08-02 18:02:42 -0500

    Has const for buttonText, hideOptions and hideCanvas also has a createCanvas function which toggles the options panel on and off.

--- frontend/src/components/Editor.js
+++ frontend/src/components/Editor.js
@@ -4,34 +4,37 @@ import '../styles/editor.scss'
 export default function Editor() {
     const [panelWidth, setCanvasWidth] = useState(16)
     const [panelHeight, setCanvasHeight] = useState(16)
+
     const [hideOptions, setHideOptions] = useState(false)
     const [hideCanvas, setHideCanvas] = useState(true)
+
+    const [buttonText, setButtonText] = useState("Create Canvas")
 
     function createCanvas() {
         setHideOptions(!hideOptions)
         setHideCanvas(!hideCanvas)
+
+        buttonText === "Create Canvas" ? setButtonText('Resize Canvas') : setButtonText("Create Canvas")
     }
 
     return (
         <div id='editor'>
             <h1>Pixel Editor</h1>
-            <h2>Enter Canvas Dimensions</h2>
+            {hideCanvas && <h2>Enter Canvas Dimensions</h2>}
 
-            <div id="options">
+            {hideCanvas && (<div id="options">
                 <div className="option">
                     <span>Height:</span>
                     <input type='number' className="panelInput" defaultValue={panelWidth} onChange={(e) => {setCanvasHeight(e.target.value)}}></input>
                 </div>
-            </div>
 
-            <div id="options">
                 <div className="option">
                     <span>Width:</span>
                     <input type='number' className="panelInput" defaultValue={panelHeight} onChange={(e) => {setCanvasWidth(e.target.value)}}></input>
                 </div>
-            </div>
+            </div>)}
 
-            <button className="button">Create</button>
+            <button className="button" onClick={createCanvas}>{buttonText}</button>
         </div>
     )
 }
