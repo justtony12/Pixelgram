2bdcc263edeb7a15b7c685e16d4e8ddbb058e791
marcus <marcus.longoria12@gmail.com>
2021-08-02 17:33:21 -0500

    default values are set by a const using 'useState' which is set to 16

--- frontend/src/components/Editor.js
+++ frontend/src/components/Editor.js
@@ -2,6 +2,9 @@ import React, {useState} from 'react'
 import '../styles/editor.scss'
 
 export default function Editor() {
+    const [panelWidth, setPanelWidth] = useState(16)
+    const [panelHeight, setPanelHeight] = useState(16)
+
     return (
         <div id='editor'>
             <h1>Pixel Editor</h1>
@@ -10,14 +13,14 @@ export default function Editor() {
             <div id="options">
                 <div className="option">
                     <span>Height:</span>
-                    <input type='number' className="panelInput" defaultValue="16"></input>
+                    <input type='number' className="panelInput" defaultValue={panelWidth}></input>
                 </div>
             </div>
 
             <div id="options">
                 <div className="option">
                     <span>Width:</span>
-                    <input type='number' className="panelInput" defaultValue="16"></input>
+                    <input type='number' className="panelInput" defaultValue={panelHeight}></input>
                 </div>
             </div>
 
