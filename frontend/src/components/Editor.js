9b7686052e4120195abdcfc17c9fd5cd81dd518f
marcus <marcus.longoria12@gmail.com>
2021-08-02 17:46:19 -0500

    Toggle function between options panel and canvas panel done.

--- frontend/src/components/Editor.js
+++ frontend/src/components/Editor.js
@@ -2,25 +2,32 @@ import React, {useState} from 'react'
 import '../styles/editor.scss'
 
 export default function Editor() {
-    const [panelWidth, setPanelWidth] = useState(16)
-    const [panelHeight, setPanelHeight] = useState(16)
+    const [panelWidth, setCanvasWidth] = useState(16)
+    const [panelHeight, setCanvasHeight] = useState(16)
+    const [hideOptions, setHideOptions] = useState(false)
+    const [hideCanvas, setHideCanvas] = useState(true)
+
+    function createCanvas() {
+        setHideOptions(!hideOptions)
+        setHideCanvas(!hideCanvas)
+    }
 
     return (
         <div id='editor'>
             <h1>Pixel Editor</h1>
-            <h2>Enter Panel Dimensions</h2>
+            <h2>Enter Canvas Dimensions</h2>
 
             <div id="options">
                 <div className="option">
                     <span>Height:</span>
-                    <input type='number' className="panelInput" defaultValue={panelWidth} onChange={(e) => {setPanelHeight(e.target.value)}}></input>
+                    <input type='number' className="panelInput" defaultValue={panelWidth} onChange={(e) => {setCanvasHeight(e.target.value)}}></input>
                 </div>
             </div>
 
             <div id="options">
                 <div className="option">
                     <span>Width:</span>
-                    <input type='number' className="panelInput" defaultValue={panelHeight} onChange={(e) => {setPanelWidth(e.target.value)}}></input>
+                    <input type='number' className="panelInput" defaultValue={panelHeight} onChange={(e) => {setCanvasWidth(e.target.value)}}></input>
                 </div>
             </div>
 
