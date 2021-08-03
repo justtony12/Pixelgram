df1253d430aa9c6908f063d0fa49b5acd150be73
marcus <marcus.longoria12@gmail.com>
2021-08-03 13:52:28 -0500

    Has routes to home/editor/feed using react-router.

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,11 +1,23 @@
+import React from 'react';
 import '../styles/App.scss'
 import Editor from './Editor'
+import Home from './Home'
+import Feed from './Feed'
+import {Route, BrowserRouter as Router, Switch, Link} from 'react-router-dom'
 
 function App() {
     return(
-        <div className='App'>
-            <Editor />
-        </div>
+        <Router>
+            <div className='App'>
+                <Link to='/'><button className='button'>Home</button></Link>
+                <Link to='/editor'><button className='button'>Editor</button></Link>
+                <Link to='/feed'><button className='button'>Feed</button></Link>
+            </div>
+
+            <Route path='/' exact component={Home}></Route>
+            <Route path='/editor' exact component={Editor}></Route>
+            <Route path='/feed' exact component={Feed}></Route>
+        </Router>
     ) 
 }
 
