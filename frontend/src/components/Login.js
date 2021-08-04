4c0841b36bf30b302bfacfdc21367b8d2662366d
marcus <marcus.longoria12@gmail.com>
2021-08-04 18:40:37 -0500

    Login page has a form.

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,11 +1,39 @@
 import React, { Component } from 'react';
 
 class Login extends Component {
-    state = {}
+    state = {
+        name: "Marcus"
+    }
+
+    handleChange = (e) => {
+        this.setState({
+            name: e.target.value
+        })
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+    }
 
     render() {
         return (
-            <div><h1>This is my Login Page!</h1></div>
+            <div>
+
+                <h1>Hello, {this.state.name}!</h1>
+                
+                <form onSubmit={this.handleSubmit} >
+                    <label>Username:</label>
+                    <input type="text" /><br/>
+
+                    <label>Email:</label>
+                    <input type="text" /><br/>
+
+                    <label>Password:</label>
+                    <input type="text" /><br/>
+
+                    <button>Submit</button>
+                </form>
+            </div>
         )
     }
 }
