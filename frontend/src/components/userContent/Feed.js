4e56854bb3c799054130d6c7b5e7a3632b3f7d74
marcus <marcus.longoria12@gmail.com>
2021-08-08 16:57:41 -0500

    created a seperate file to house the post list.

--- frontend/src/components/userContent/Feed.js
+++ frontend/src/components/userContent/Feed.js
@@ -1,30 +1,15 @@
 import React, { Component } from 'react';
-import '../editing/styles/home.scss';
 import { connect } from 'react-redux';
+import { fetchPost } from './actions/postActions';
+import postList from './postList';
+
+
+import '../editing/styles/home.scss';
 import { Link } from 'react-router-dom'
 
 class Home extends Component {
     render() {
-        const { posts } = this.props;
-
-        const postList = posts.length ? (
-            posts.map(post => {
-                return (
-                    <div className='postCard' key={post.id}>
-                        <Link to={'/post/' + post.id} ><span>{post.art}</span></Link>
-                        <p>{post.caption}</p>
-                    </div>
-                )
-            })
-        ) : (<div>You do not have any posts yet...</div>)
-
-        return (
-            <div>
-                <h1>Welcome User!</h1>
-                <Link to={'/create'}><button className='button' >New Post</button></Link>
-                {postList}
-            </div>
-        )
+        return (null)
     }
 }
 
---
+++ frontend/src/components/userContent/postList.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class PostList extends React.Component {
+    listPosts = () => {
+        return this.props.art.map(post => <img key={post.id} src={post.url} alt={post.id} />)
+    }
+
+    render() {
+        return(
+            <div>
+                {this.listPosts}
+            </div>
+        )
+    }
+}
+
+export default PostList;
