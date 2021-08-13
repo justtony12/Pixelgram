04ce7d351ef8ae59f7f1940323071ac722e33c65
marcus <marcus.longoria12@gmail.com>
2021-08-13 11:32:12 -0500

    Un-used folders/files have been deleted.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,28 @@
 import React, { Component } from 'react';
-import Navbar from './components/layout/Navbar';
 import './components/editing/styles/App.scss';
+import { Route, BrowserRouter as Router, Switch, Link } from 'react-router-dom';
+import Home from './components/posts/Home';
+import Editor from './components/editing/Editor';
+import Create from './components/posts/CreatePost';
+import Post from './components/posts/PostSummary';
 
 class App extends Component {
     render() {
         return (
-            <div className='app'>
-                <Navbar />
+            <Router>
+            <div className='Navbar'>
+                <Link to='/'><button className='button'>Home</button></Link>
+                <Link to='/editor'><button className='button'>Creator</button></Link>
+                <Link to='/create'><button className='button'>Upload</button></Link>
             </div>
+
+            <Switch>
+                <Route exact path='/' component={Home} />
+                <Route exact path='/editor' component={Editor} />
+                <Route exact path='/create'component={Create} />
+                <Route exact path='/post/:post_id'component={Post} />
+            </Switch>
+        </Router>
         );
     }
 }
